e12bbbca9d6d60342d39f099a55e174d039f5a3b
rad-a <11688089+rad-a@users.noreply.github.com>
2020-07-06 20:50:44 -0500

    merging express.js and reservation.js files

--- express.js
+++ express.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
+const reservation = require("reservation.js");
 
 
 // Sets up the Express app to handle data parsing
@@ -10,7 +11,54 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 
+const reservationList = [
+    {
+        routeName: "make",
+        ID: "reservationID",
+        name: "MakeReservation",
+        email: "reserve@mail.com",
+        phone: "000-000-0000"
+    }
+];
 
+const waitList = [
+    {
+        routeName: "make",
+        ID: "reservationID",
+        name: "MakeReservation",
+        email: "reserve@mail.com",
+        phone: "000-000-0000"
+    }
+]
+
+//Default AJAX page user sees
+app.get("/", function(req, res) {
+    res.sendFile(path.join(_dirname, "index.html"))
+});
+
+app.get("/make", function(req, res) {
+    res.sendFile(path.join(__dirname, "/make.html"))
+});
+
+//Display all reservations
+app.get("api/tables", function(req, res) {
+    return res.json(reservationList);
+});
+
+//Create a new reservation from JSON input
+app.post("api/reservationList", function(req, res) {
+    let newReservation = req.body;
+
+
+    newReservation.routeName = newCharacter.id.replace(/\s+/g, "").toLowerCase();
+
+    console.log(newReservation);
+
+    reservationList.push(newCharacter);
+
+    res.json(newReservation);
+
+})
 
 
 
