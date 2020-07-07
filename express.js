1d098702084860b05b63a7e922076ac8298ee10c
Mark McDonald <markwilliammcdonald@gmail.com>
2020-07-06 19:28:03 -0500

    added in the dependecies and the express.js file

---
+++ express.js
@@ -0,0 +1,10 @@
+// Dependencies
+// =============================================================
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
